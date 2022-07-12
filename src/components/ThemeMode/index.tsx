e4b3301a7f9df6b41ca37bbf7d5a2dcd82701a01
chlee1001 <chlee1001@naver.com>
2022-07-12 19:42:29 +0900

    :art: 테마모드를 변경하는 컴포넌트 추가

---
+++ src/components/ThemeMode/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+import useTheme from '#hooks/useTheme'
+import HalfMoon from '#assets/static/images/half-moon.png'
+import useLanguage from '#hooks/useLanguage'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const LanguageSelect = styled.select`
+  -webkit-appearance: none;
+  cursor: pointer;
+`
+
+const ThemeContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const ThemeButton = styled.button`
+  width: 1.625rem;
+  height: 1.625rem;
+
+  background-image: url(${HalfMoon});
+  background-repeat: no-repeat;
+  background-size: 100%;
+`
+
+const ThemeMode = () => {
+  const { toggleDarkTheme } = useTheme()
+  const { currentLanguage, changeLanguage } = useLanguage()
+
+  return (
+    <Container className="container">
+      <div className="language">
+        <LanguageSelect
+          defaultValue={currentLanguage}
+          onChange={changeLanguage}
+        >
+          <option value="Korean">Korean 🇰🇷</option>
+          <option value="English">English 🇺🇸</option>
+        </LanguageSelect>
+      </div>
+      <ThemeContainer className="theme-container">
+        <ThemeButton type="button" onClick={toggleDarkTheme} />
+      </ThemeContainer>
+    </Container>
+  )
+}
+
+export default ThemeMode
