721cbe2fb5f448fc870778b3f8cdd9a98a3717a9
chlee1001 <chlee1001@naver.com>
2022-07-12 11:06:04 +0900

    :lipstick: global style적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import GlobalStyles from '#styles/global-styles'
+import GlobalStyle from '#styles/global-style'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <BrowserRouter>
-    <>
-      <GlobalStyles />
-      <App />
-    </>
-  </BrowserRouter>,
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
 )
