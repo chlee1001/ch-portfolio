b75de90e5576922cfc5c74b20c9ba6eefe4f1d67
chlee1001 <chlee1001@naver.com>
2022-07-12 19:41:43 +0900

    :art: theme/language context provider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React, { FC } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from '#pages/home-page'
+import { Helmet } from 'react-helmet'
+
+import myData from '#assets/db.json'
+import { ThemeChangeContextProvider } from '#context/theme-change-context'
+import Home from '#pages/Home'
+import { LanguageContextProvider } from '#context/language-context'
 
 const App: FC = () => {
+  const {
+    meta: { title },
+  } = myData
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <ThemeChangeContextProvider>
+        <LanguageContextProvider>
+          <Home />
+        </LanguageContextProvider>
+      </ThemeChangeContextProvider>
+    </>
   )
 }
 
