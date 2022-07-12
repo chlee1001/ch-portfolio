07f4672569b523019d2672714140e12909422072
chlee1001 <chlee1001@naver.com>
2022-07-12 11:02:07 +0900

    :lipstick: 글로벌 스타일 변수 값 추가

---
+++ src/styles/theme/mixins.tsx
@@ -0,0 +1,87 @@
+import { keyframes } from 'styled-components'
+import * as styleVariable from './variables'
+
+export const mixins = {
+  tagYellow: () => `
+    display: flex;
+    width: fit-content;
+    background: ${styleVariable.color.primaryOpacity};
+    color: ${styleVariable.color.primary};
+    border-radius: ${styleVariable.borderRadius};
+    font-size: ${styleVariable.size.small1};
+    font-weight: ${styleVariable.weight.semiBold};
+    padding: 0 5px;
+    margin-right: 0.5rem;
+    min-width: 4rem;
+    justify-content: center;
+  `,
+
+  subTitle: () => `
+    border-radius: 0.25rem;
+    color: #ffaf19;
+    width: fit-content;
+    padding: 0 1rem;
+    font-size: 1.125rem;
+    font-weight: 700;
+    margin-bottom: 0.775rem;
+    border: 1px solid #ffaf19;
+    box-shadow: 1px 1px 1px;
+  `,
+
+  underline: () => `
+    content: '';
+    width: 100%;
+    height: 6px;
+    background: ${styleVariable.color.primary};
+    position: absolute;
+    bottom: 3px;
+    left: 0;
+    opacity: 0.5;
+    z-index: -1;
+  `,
+
+  expert: () => `
+    border: 1px solid ${styleVariable.color.primaryEmphasis};
+    color: ${styleVariable.color.primaryEmphasis};
+    background: ${styleVariable.color.primaryEmphasisOpacity};
+  `,
+
+  techTags: () => `
+    width: fit-content;
+    padding: 0.1rem 0.25rem;
+    border-radius: ${styleVariable.borderRadius};
+    border: 1px solid ${styleVariable.color.grey8};
+    color: ${styleVariable.color.grey8};
+    font-size: ${styleVariable.size.small1};
+    line-height: 1;
+    height: fit-content;
+    margin: 0.225rem 0.225rem 0 0;
+  `,
+}
+
+export const commonKeyframes = {
+  blink: keyframes`
+    50% {
+      border-color: ${styleVariable.color.primary};
+    }
+  `,
+
+  shine: keyframes`
+    0% {
+      background-position: 0;
+    }
+    60% {
+      background-position: 200px;
+    }
+    100% {
+      background-position: 400px;
+    }
+  `,
+}
+
+export const commonDevices = {
+  mobile: `@media (${styleVariable.devices.mobileS} and max-width: ${styleVariable.breakpoints.tablet} -1px)`,
+  tablet: `@media (${styleVariable.devices.tablet} and max-width: ${styleVariable.breakpoints.laptop} -1px)`,
+  laptop: `@media (${styleVariable.devices.laptop} and max-width: ${styleVariable.breakpoints.desktop} -1px)`,
+  desktop: `@media (${styleVariable.devices.desktop}`,
+}
---
+++ src/styles/theme/variables.tsx
@@ -0,0 +1,98 @@
+export const borderRadius = '1.25rem'
+export const borderRadiusLight = '0.25rem'
+
+export const size = {
+  huge1: '5rem',
+  huge0: '4rem',
+  large15: '3rem',
+  large14: '2.825rem',
+  large13: '2.75rem',
+  large12: '2.625rem',
+  large11: '2.5rem',
+  large10: '2.375rem',
+  large7: '2rem',
+  large6: '1.875rem',
+  large5: '1.75rem',
+  large4: '1.625rem',
+  large3: '1.5rem',
+  large2: '1.375rem',
+  large1: '1.25rem',
+  large0: '1.125rem',
+  default: '1rem',
+  small0: '0.875rem',
+  small1: '0.75rem',
+  small2: '0.625rem',
+  small3: '0.5rem',
+}
+
+export const weight = {
+  thin: '100',
+  light: '300',
+  regular: '400',
+  medium: '500',
+  lightBold: '600',
+  semiBold: '700',
+  bold: '800',
+  black: '900',
+}
+
+export const color = {
+  primary: 'rgb(255, 175, 25)',
+  primaryLight: 'rgb(255, 247, 234)',
+  primaryOpacity: 'rgba(255, 175, 25, 0.1)',
+  primaryOpacityHeavy: 'rgba(255, 175, 25, 0.2)',
+  primaryOpacityHeavyLight: 'rgb(255, 231, 185)',
+  primaryEmphasis: 'rgb(255, 98, 26)',
+  primaryEmphasisOpacity: 'rgb(255, 98, 26, 0.1)',
+  primaryEmphasisLight: 'rgb(255, 230, 219)',
+  alert: 'rgb(255, 84, 0)',
+  alertOpacity: 'rgba(255, 84, 0, 0.15)',
+  alertOpacityLight: 'rgb(254, 230, 218)',
+  safe: 'rgb(0, 150, 4)',
+  safeOpacity: 'rgba(0, 150, 4, 0.1)',
+  safeOpacityLight: 'rgb(229, 244, 229)',
+  darkEvent: 'rgb(26, 75, 255)',
+  darkEventOpacity: 'rgba(26, 75, 255, 0.1)',
+  darkEventOpacityLight: 'rgb(232, 237, 255)',
+  main: '#222',
+  white: '#fff',
+  greyLight: 'rgb(250, 250, 250)',
+  grey0: 'rgb(240, 240, 240)',
+  grey1: 'rgb(239, 240, 241)',
+  grey2: 'rgb(227, 230, 233)',
+  grey3: 'rgb(216, 224, 231)',
+  grey4: 'rgb(204, 213, 221)',
+  grey5: 'rgb(188, 199, 211)',
+  grey6: 'rgb(167, 178, 190)',
+  grey7: 'rgb(156, 160, 165)',
+  grey8: 'rgb(138, 142, 148)',
+  grey9: 'rgb(118, 123, 131)',
+  grey10: 'rgb(91, 95, 102)',
+  grey11: 'rgb(63, 66, 71)',
+}
+
+export const backgroundColor = {
+  main: '#222',
+}
+
+export const boxShadow = '0 0.08em 0.35em rgb(0 0 0 / 18%)'
+
+export const breakpoints = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+}
+
+export const devices = {
+  mobileS: `(min-width: ${breakpoints.mobileS})`,
+  mobileM: `(min-width: ${breakpoints.mobileM})`,
+  mobileL: `(min-width: ${breakpoints.mobileL})`,
+  tablet: `(min-width: ${breakpoints.tablet})`,
+  laptop: `(min-width: ${breakpoints.laptop})`,
+  laptopL: `(min-width: ${breakpoints.laptopL})`,
+  desktop: `(min-width: ${breakpoints.desktop})`,
+}
