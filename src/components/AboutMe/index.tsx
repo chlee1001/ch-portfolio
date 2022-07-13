b6552d9ae4b431d61e3a5db0925b32a54cc85ca4
chlee1001 <chlee1001@naver.com>
2022-07-13 13:54:48 +0900

    :lipstick: 모바일에서 aboutme 영억 하단 여백이 줄어들도록 수정

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -4,24 +4,18 @@ import styled, { css } from 'styled-components'
 import SubTitle from '#components/commons/SubTitle'
 import PersonalInfos from '#components/AboutMe/personal-infos'
 import Introduce from '#components/AboutMe/introduce'
-import { maxDevices, minDevices } from '#styles/theme/variables'
+import { maxDevices } from '#styles/theme/variables'
 
 const AboutMeContainer = styled.div`
   padding: 0 1rem;
   margin-bottom: 5rem;
   user-select: none;
 
-  @media ${minDevices.mobileL} and ${maxDevices.tablet} {
-    ${css``}
+  @media ${maxDevices.mobileL} {
+    ${css`
+      margin-bottom: 3rem;
+    `}
   }
-
-  @media ${minDevices.mobileM} and ${maxDevices.mobileL} {
-    ${css``}
-  }
-  @media ${minDevices.mobileS} and ${maxDevices.mobileM} {
-    ${css``}
-  }
-
   @media ${maxDevices.mobileXS} {
     ${css`
       padding: 0;
