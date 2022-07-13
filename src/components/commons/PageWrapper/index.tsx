a1613da629761555622294f027071ffec98ca9d2
chlee1001 <chlee1001@naver.com>
2022-07-13 11:03:36 +0900

    :lipstick: 테마모드 전환 시 fade효과 추가

--- src/components/commons/PageWrapper/index.tsx
+++ src/components/commons/PageWrapper/index.tsx
@@ -11,6 +11,46 @@ const Page = styled.div`
   margin: 0 auto;
   padding: 2rem 0;
   height: 100%;
+
+  &.fade-enter {
+    opacity: 0;
+    animation: slideIn 0.6s;
+  }
+  &.fade-enter-active {
+    opacity: 1;
+    transition: opacity 500ms;
+  }
+  &.fade-exit {
+    opacity: 0;
+    animation: slideOut 0.6s;
+  }
+  &.fade-exit-active {
+    opacity: 1;
+    transition: opacity 500ms;
+  }
+
+  @keyframes slideIn {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      filter: blur(10px);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slideOut {
+    0% {
+      opacity: 0;
+      filter: blur(0px);
+    }
+
+    100% {
+      filter: blur(10px);
+      opacity: 1;
+    }
+  }
 `
 
 const PageContainer = styled.div`
